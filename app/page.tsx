48ffab525a42337c0e5ca212bcadf78e4b2d4611
Soulace <Soulace@Soulaces-MacBook-Pro.local>
2022-12-03 19:11:57 -0800

    troubleshooting type error in app - page

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { TiDelete } from "react-icons/ti"
 import { FiEdit3 } from "react-icons/fi"
 import Link from 'next/link';
 
-type Entry = {
+interface Entry extends Object {
   "id":string,
   "collectionId":string,
   "collectionName":string,
@@ -16,19 +16,22 @@ type Entry = {
   "journalDate_created":string,
   "journalDate_updated":string,
   "entryOwner":"00fnjkgjlt7fskx"
-}
+};
 
 export default function HomePage() {
   const pb = new PocketBase('http://127.0.0.1:8090');
-  const [entries, setEntries] = useState<Record<string, Entry>[]>();
+  const [entries, setEntries] = useState<Entry[]>();
 
   const journalFetch = async() => {
     const records = await pb.collection('entries').getFullList(200 , {
       sort: '-created',
       '$autoCancel': false
+      }).then((res)=>{
+      // @ts-ignore --> Get back to it
+        setEntries(res)
+      }).catch((err) => {
+        return `Error! ${err}`
       })
-    const res = records
-    setEntries(records)
   }
   
   useEffect(() => {
@@ -47,6 +50,7 @@ export default function HomePage() {
     })
   }
 
+  console.log(entries)
   return (
     <div className="text-4xl">
       <h1 className="text-white text-center py-4"> Welcome back, James</h1>
@@ -55,16 +59,16 @@ export default function HomePage() {
         {entries?.map((entry) => {
           return (
             <div className="flex align-middle bg-zinc-100 bg-opacity-40 w-2/5 my-3 p-2 rounded-lg" key={entry.id}>
-              <h2 className="text-base text-white truncate w-10/12">
-                {entry.title}
-              </h2>
-              <Link href="/editentry" className="w-1/12 flex cursor-pointer justify-center">
-                <FiEdit3 size={24}/>
-              </Link>
-              <div className="w-1/12 flex cursor-pointer justify-center" onClick={(e) => deleteEntry(e, entry.id)}>
-                <TiDelete size={24}/>
-              </div>
+            <h2 className="text-base text-white truncate w-10/12">
+              {entry.title}
+            </h2>
+            <Link href="/editentry" className="w-1/12 flex cursor-pointer justify-center">
+              <FiEdit3 size={24}/>
+            </Link>
+            <div className="w-1/12 flex cursor-pointer justify-center" onClick={(e) => deleteEntry(e, entry.id)}>
+              <TiDelete size={24}/>
             </div>
+          </div>
           )
         })}
 
