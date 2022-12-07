0fcd59e082e917392e36462fae7174fd2a48232c
Soulace <Soulace@Soulaces-MacBook-Pro.local>
2022-12-06 16:03:05 -0800

    created individual get req in entry

--- app/entry/[id]/page.tsx
+++ app/entry/[id]/page.tsx
@@ -3,10 +3,28 @@
 import { useState, useEffect } from "react";
 import PocketBase from 'pocketbase';
 
-export default function IDEntry(id:string) {
+interface ID extends Object {
+  params: {'id': string},
+  searchParams: object
+}
+
+interface Entry extends Object {
+  'id': string,
+  "collectionId": string,
+  "collectionName": string,
+  "created": Date,
+  "updated": Date,
+  "title": string | undefined,
+  "body": string | undefined,
+  "journalDate_created": Date,
+  "journalDate_updated": Date,
+  "entryOwner": string
+}
+
+export default function IDEntry(id:ID) {
   const pb = new PocketBase('http://127.0.0.1:8090');
   const d = new Date()
-  const [entry, setEntry] = useState()
+  const [entry, setEntry] = useState<Entry>()
   const [newTitle, setNewTitle] = useState('');
   const [newBody, setNewBody] = useState('');
   const [day, setDay] = useState(0);
@@ -15,15 +33,16 @@ export default function IDEntry(id:string) {
   const [hour,setHour] = useState(0);
   const [minute, setMinute] = useState(0);
   const [second, setSecond] = useState(0);
-
+  const entryId:string = id.params.id
 
   const journalFetch = async() => {
-    const records = await pb.collection('entries').getOne(`${id}`)
+    const records = await pb.collection('entries').getOne(`${entryId}`)
     .then((res)=>{
-      // @ts-ignore
-        setEntry(res)
+        console.log(res);
+        // @ts-ignore
+        setEntry(res);
       }).catch((err) => {
-        return `Error! ${err}`
+        return `Error! ${err}`;
       })
   }
   
@@ -31,7 +50,7 @@ export default function IDEntry(id:string) {
     journalFetch();
   },[])
 
-    useEffect(()=>{
+  useEffect(()=>{
     setInterval(()=>{
       setSecond(d.getUTCSeconds());
       setMinute(d.getUTCMinutes());
@@ -42,29 +61,23 @@ export default function IDEntry(id:string) {
     }, 1000)
   })
 
-  
-  const fullDateUTC = new Date(Date.UTC(year, month, day, hour, minute, second));
   const date = `${month + 1}/${day}/${year}`
-
-
 
 
   return (
     <div className="flex flex-col items-center">
       <div className="w-3/5 px-6">
-        <form action="/" method="post" className="flex flex-col items-center h-screen">
+        <form action="/" method="put" className="flex flex-col items-center h-screen">
 
           <label htmlFor="title" className="text-xl text-white text-left my-4 w-full">           
             <p className="text-white text-lg" >{date}</p>
           </label>
-
-          <input type="text" id="title" name="title" placeholder="Title" className="w-full rounded-lg mb-4 p-4" onChange = {(e) => setNewTitle(e.target.value)}>
-            {/* {entry.title} */}
-          </input>
+          <input type="text" id="title" name="title" placeholder={entry?.title? entry.title : "Title" } className="w-full rounded-lg mb-4 p-4" onChange = {(e) => setNewTitle(e.target.value)} />
 
           <label htmlFor="body"/>
+
           <textarea id="body" name="body" placeholder="What's on your mind?" className="w-full rounded-lg mb-6 p-4 h-3/6" onChange = {(e) => setNewBody(e.target.value)}>
-            {/* {entry.body} */}
+            {entry?.body}
           </textarea>
           
           <button type="submit" className="self-end bg-zinc-800 p-3 text-white rounded-lg">Save</button>
